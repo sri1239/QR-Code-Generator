9bcd1f9af2a7440820ffc225d9e77abb113aa83c
Kavya <kavyachalla5@gmail.com>
2025-06-22 12:16:44 +0530

    first-commit

---
+++ server/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+const router = require('./routes');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static(path.join(__dirname, '../client')));
+app.use(router);
+
+app.listen(port,()=>{
+    console.log(`Server listening on Port ${port}`)
+})
